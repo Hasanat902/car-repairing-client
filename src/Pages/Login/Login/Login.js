6b1ce9b482d3765189de6beafdc965f910e9f4cc
Hasanat902 <abulhasanat902@gmail.com>
2022-09-13 16:22:57 +0600

    added jwt token

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -8,6 +8,7 @@ import SocialLogin from "../SocialLogin/SocialLogin";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import PageTitle from "../../Shared/PageTitle/PageTitle";
+import axios from "axios";
 
 const Login = () => {
 
@@ -38,16 +39,20 @@ const Login = () => {
       }
 
       if(user){
-        navigate(from, {replace: true});
+        
       }
 
-    const handleSubmit = event => {
+    const handleSubmit = async event => {
         event.preventDefault();
 
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
 
-        signInWithEmailAndPassword(email, password);
+        await signInWithEmailAndPassword(email, password);
+        
+        const {data} = await axios.post('http://localhost:5000/login', {email});
+        localStorage.setItem('accessToken', data.accessToken);
+        navigate(from, {replace: true});
     }
 
     const resetPassword = async() => {
--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,9 +1,41 @@
+import { signOut } from 'firebase/auth';
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import axiosPrivate from '../../api/axiosPrivate';
+import auth from '../../firebase.init';
 
 const Orders = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOrders] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect( () => {
+        
+        const getOrders = async() => {
+            const email = user.email;
+            const url = `http://localhost:5000/order?email=${email}`;
+            try{
+                const {data} = await axiosPrivate.get(url);
+                setOrders(data);
+            }
+            catch(error){
+                if(error.response.status === 401 || error.response.status === 403){
+                    signOut(auth);
+                    navigate('/login')
+                }
+            }
+        }
+
+        getOrders();
+
+    }, [])
+
     return (
         <div>
-            
+            <h2>Your Orders: {orders.length}</h2>
         </div>
     );
 };
